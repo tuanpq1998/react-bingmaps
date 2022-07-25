98a185a5c4459bcbdb5b46dc8edd095ea159e022
tuanpq1998 <tuanquang123321@gmail.com>
2022-07-25 22:53:03 +0700

    update react version to test in strict mode

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
